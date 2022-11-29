e05c5e82037844a3c84527450f9c0f86ba5dd1ca
PB <66083376+PBmachine@users.noreply.github.com>
2022-11-28 20:47:17 -0500

    Updated import
    import now sorts mesh

--- pysliceMain/psMeshimport.py
+++ pysliceMain/psMeshimport.py
@@ -7,47 +7,29 @@
 #  ====   _  ===  =   ==  ==     ==  ==   =  ==   _
 #  ==      ====    ====   =====  ==   ====   ======  
 # 
-################################################### 
-#  .venv\scripts\activate
-# #                    
+###################################################                
 # Function:
 # opens stl binary mesh file and creates mesh with facets
-# creates mesh class containing file header, num facets, list of facets, bounding box
+# creates mesh class containing file header, num facets,
+#   dict of facets stored by key (zmin,zmax), list of keys, bounding box
 # facet class contains normal vector, array of xyz for vertices, bounding box 
-#
-# To Do:
-# !: write code for sorting, transforming?
-# maybe: alternative parsing for ascii format
+# function for homogenizing coordinated - row or column major
+# 
+# Next steps: 
+# review data sort and store method
+# ascii file handler
 ###################################################
-
-#Binary STL format https://docs.fileformat.com/cad/stl/
-# 80 byte header
-# 4 byte u_int number of facets
-# - for all facets:
-# 4 byte, 4 byte, 4 byte - i, j, k float for normal
-# 4 byte, 4 byte, 4 byte - x, y, z float for vertex 1
-# 4 byte, 4 byte, 4 byte - x, y, z float for vertex 2
-# 4 byte, 4 byte, 4 byte - x, y, z float for vertex 3
-# 2 byte u_int for attriute byte count (usually null? I think it's unimportant....
-# - just ends at the end of the last facet
-
-# may need a handler for determining if file is valid and in what format
-# valid binary file should have filesize = num facets *bytes per facet + fileInfo bytes
 import struct
 import os
 import numpy as np
 
-#make a numpy array for facet vertices
-#numpy.zeros(shape, dtype=float, order='C', *, like=None)
-#https://www.geeksforgeeks.org/data-type-object-dtype-numpy-python/?ref=gcse
-
-##Classes
+##>>>>> --------------- Classes ------------------------------------------------------------ <<<<<
 class Facet(object):
     def __init__(self, normal, v, a):
-        #A facet with a normal and three vertices + some other stuff
+        #A facet with a normal and three vertices + 
         self.normal = np.array(normal)
         self.v = np.mat(v)
-        self.a = a #attributes - may use to label later
+        self.a = a #attributes - unused in standard STL format
         self.bbox = np.zeros((2,3))
     
     def facetPrint(self):
@@ -55,6 +37,14 @@ class Facet(object):
         print(self.normal)
         print('Vertices:')
         print(self.v)
+
+    def getbBox(self):
+        maxpts = np.amax(self.v, axis = 0)
+        minpts = np.amin(self.v, axis = 0)
+        self.bbox[0] = minpts
+        self.bbox[1] = maxpts
+        print(f' bounding box', self.bbox)
+        return self.bbox
         
     def printVertex(self,vertex):
         return(f'x:{vertex[0]}, y:{vertex[1]}, z:{vertex[2]}')
@@ -65,44 +55,71 @@ class Mesh():
         self.fName = fName
         self.header = fInfo[0]
         self.numfacets = fInfo[1]
-        self.facets =  [] #should reformat so I can use array
         self.fcoord = np.zeros((self.numfacets,3,3)) #3d array of all facet coordinates 
         self.bbox = np.zeros((2,3))
+        self.dfacets = dict() #all mesh facets stored by key = (zmin,zmax)
+        self.lookup = [] #sorted list of facet keys 
     
     def getFacets(self):
         #iterates through num facets and makes facets
         for i in range(self.numfacets):
-            normal = rVector(self.fName,'float')
-            v0 = rVector(self.fName,'float')
-            v1 = rVector(self.fName,'float')
-            v2 = rVector(self.fName,'float')
+            normal = rVector(self.fName,'floatR')
+            v0 = rVector(self.fName,'floatR')
+            v1 = rVector(self.fName,'floatR')
+            v2 = rVector(self.fName,'floatR')
             a = struct.unpack('H', self.fName.read(2))[0]
-            self.facets.append(Facet(normal, [v0,v1,v2], a))
-            self.fcoord[i] = np.asmatrix(self.facets[i].v) 
-    
+
+            #create facet and store in dict and matrix
+            facet = Facet(normal, [v0,v1,v2], a)
+            self.addToFacetDict(facet)
+            self.fcoord[i] = np.asmatrix([v0,v1,v2]) 
+
+    def addToFacetDict(self,facet):
+        #create facet label as zmin,zmax and store in dict
+        #for non-z slicing need generalization or temporarily rotate model to z plane   
+        facet.getbBox()
+        l = tuple((facet.bbox[0,2], facet.bbox[1,2]))
+
+        if l in self.dfacets: 
+            self.dfacets[l] += [facet]
+        else:
+            self.dfacets[l]=[facet]
+
+    def facetkeyList(self):
+        #creates sorted lookup list by zmin values
+        self.lookup = sorted(self.dfacets.keys())
+        return self.lookup
+
     def printFacetInfo(self,lo=0,hi=-1):
         #prints normals and vertices for all facets in mesh or in selected range
         if hi == -1: hi = self.numfacets
         for i in range(lo,hi):
             print(f'\nFacet {i} -')
-            self.facets[i].facetPrint()
+            f = self.nFacet(i)
+            f.facetPrint
+    
+    def nFacet(self,n):
+        #returns the nth facet of the mesh
+        i = 0
+        while i <= n:
+            if i == n:
+                return self.dfacets[self.lookup[i]][0]
+            else: 
+                l = len(self.lookup[i])
+                if i+l>n:
+                    return self.nFacet(n-i)
+                i += l
 
     def getBBox(self):
-        #base version to find bounding box - fix for efficiency
-        #iterate through all vertices and find highest and lowest
-        for i in range(self.numfacets):
-            facet = self.facets[i]
-            maxpts = np.amax(facet.v, axis = 0)
-            minpts = np.amin(facet.v, axis = 0)
-            facet.bbox = [minpts,maxpts]
-
+        #find max and min pts
         self.bbox[0] = np.amin(self.fcoord,axis = (0,1))
         self.bbox[1] = np.amax(self.fcoord,axis = (0,1))
+        return self.bbox
 
 
 
 ##>>>>> --------------- Functions ------------------------------------------------------------ <<<<<
-#https://docs.python.org/3/library/struct.html struct info
+
 def rFloat(fName):
     bytes8 = fName.read(4)
     return struct.unpack('f', bytes8)[0]
@@ -114,11 +131,14 @@ def rVector(fName, vtype):
         a = rFloat(fName)
         b = rFloat(fName)
         c = rFloat(fName)
+    elif vtype == 'floatR': #4 dec round to aide future calculations 
+        a = round(rFloat(fName), 4)
+        b = round(rFloat(fName), 4)
+        c = round(rFloat(fName), 4)
     return[a,b,c]
 
 def homog(verts):
     print(f'starting shape:{np.shape(verts)}')
-    
     new = np.transpose(verts)
     new = np.append(new,[[1,1,1]], axis = 0)
     print(f'resulting shape:{np.shape(new)}')
@@ -150,12 +170,33 @@ def openSTL(filepath):
         return 
     myMesh = Mesh(mf,info)
     myMesh.getFacets()
+    myMesh.facetkeyList()
     myMesh.getBBox()
     print('completed parse')
     return myMesh
 
+##>>>>> --------------- Additional ------------------------------------------------------------ <<<<<
 
-# filepath = "pysliceMain\Mesh_Models\Cone_10x10_96_bin.stl"
-# def importMSH(filepath):
+def testcone():
+    #test function to check code
+    filepath = "Mesh_Models\\Cone_10x10_96_bin.stl"
+    myMesh = openSTL(filepath)
+    myMesh.printFacetInfo(0,1)
+    print(f'\nMesh Bounding Box: {myMesh.bbox}')
+    F0 = myMesh.nFacet(0).v
+    print(f'homogenized coordinate\n {homog(F0)}')
 
-#run()
+#Binary STL format https://docs.fileformat.com/cad/stl/
+# 80 byte header
+# 4 byte u_int number of facets
+# - for all facets:
+# 4 byte, 4 byte, 4 byte - i, j, k float for normal
+# 4 byte, 4 byte, 4 byte - x, y, z float for vertex 1
+# 4 byte, 4 byte, 4 byte - x, y, z float for vertex 2
+# 4 byte, 4 byte, 4 byte - x, y, z float for vertex 3
+# 2 byte u_int for attriute byte count (usually null? I think it's unimportant...
+
+# valid binary file should have filesize = num facets *bytes per facet + fileInfo bytes
+
+#numpy matrices
+#https://www.geeksforgeeks.org/data-type-object-dtype-numpy-python/?ref=gcse
--- pysliceMain/psSlicer.py
+++ pysliceMain/psSlicer.py
@@ -16,6 +16,44 @@
 ###################################################
 
 import numpy as np
+import psMeshimport as msh
+
+
+"""
+Slice loop
+define a z cutting plane
+
+loop - over all facets or facets in range
+find facets that intersect with that plane
+
+loop-until all facets cycled through
+calculate the intersection contour
+store the intersection contour as vector in the slice
+
+loop 
+sort line segment vectors to construct layer contour
+label and store as slice
+
+continue until final z height reached
+
+"""
+#plane ax+by+cz+d=0 
+#normal = V1 x V2 
+
+
+def slicebyZ(mesh, h, offset = 0.00001):
+    z = mesh.bbox()[0,2]
+    count = 0
+    while zh < (mesh.bbox()[1,2]+offset):
+        zh = round(z,4)+offset #ensures non intersection with vertices
+        sliceAt(mesh,zh)
+        z += h
+        count += 1
+    print(f'slicing complete: created {count} slices')
+
+
+def sliceAt(mesh,zh):
+    pass
 
 
 
@@ -23,13 +61,17 @@ class slice(object):
     def __init__(self,seq):
         self.seq = seq
         self.points = np.array()
-        self.ocontour = []
-        self.icontour= []
+        self.contour = []
+
+class countour(object):
+    def __init__(self):
+        self.points = []
+        self.type = ''
 
 class infill(object):
+    #infill is not included in this version
     pass
-
-        
+       
 
 class subMesh(object):
     def __init__():
